58f3059bf74117c482ae3300176fa0abe55c5fb5
Umut Soysal <umut.soysal@ibtech.com.tr>
2021-08-19 17:28:02 +0300

    initial commit

---
+++ app/src/main/java/com/laks/mobile/composelazylistscrolleffects/data/ImageDataModel.kt
@@ -0,0 +1,7 @@
+package com.laks.mobile.composelazylistscrolleffects.data
+
+data class ImageDataModel(
+    val id: Int,
+    val title: String,
+    val imageUrl: String
+)
---
+++ app/src/main/java/com/laks/mobile/composelazylistscrolleffects/util/ComponentExtensions.kt
@@ -0,0 +1,13 @@
+package com.laks.mobile.composelazylistscrolleffects.util
+
+import androidx.compose.foundation.lazy.LazyListLayoutInfo
+
+
+fun LazyListLayoutInfo.normalizedItemPosition(key: Any): Float =
+    visibleItemsInfo
+        .firstOrNull { it.key == key }
+        ?.let {
+            val center = (viewportEndOffset + viewportStartOffset - it.size) / 2F
+            (it.offset.toFloat() - center) / center
+        }
+        ?: 0F
